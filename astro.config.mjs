9c34752c52e305219fd3e1772e801155bd82275b
Daniel Castañeda <danielinocp@gmail.com>
2025-03-02 17:21:32 -0500

    fix(astroconfig):link redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
-  site:`https://example.com`,
+  site:`https://astrostaticblog.netlify.app`,
    vite: {
     plugins: [tailwindcss()],
   },
